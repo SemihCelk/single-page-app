662b4e1c17cb3c71108caab76c0e975e3f767af2
semih <msemihcelik62@gmail.com>
2024-01-26 19:26:04 +0300

    feat: allows image link

--- web-app/next.config.mjs
+++ web-app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['i.hizliresim.com'],
+      },
+};
 
 export default nextConfig;
+ 
