597301220dd73ca48ffca3e3fd284e47f0e3ef6d
semih <msemihcelik62@gmail.com>
2024-01-27 18:53:18 +0300

    refactor: button component

--- web-app/src/components/buttons/button.tsx
+++ web-app/src/components/buttons/button.tsx
@@ -19,7 +19,7 @@ const Button = ({
   hasIcon,
   className,
 }: ButtonProps) => {
-  const buttonClassName = `px-6 py-3 rounded-lg border-2 border-amber-900 text-amber-900 text-base font-semibold font-roboto tracking-wide ${
+  const buttonClassName = ` py-3 rounded-lg border-2 border-amber-900 text-amber-900   ${
     hasIcon ? "flex items-center" : ""
   } ${className || ""}`;
 
